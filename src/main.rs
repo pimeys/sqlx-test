8403a8ae0bcf3d97c9f2d85135035346779e8485
Julius de Bruijn <julius+github@nauk.io>
2020-07-02 22:20:16 +0200

    Writes maybe like this?

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,27 @@ use sqlx::{
     Connect, Postgres, Row, Type,
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Value {
     Integer(Option<i64>),
     Float(Option<f64>),
     Array(Option<Vec<Value>>),
+}
+
+impl Value {
+    fn to_int(self) -> Option<i64> {
+        match self {
+            Self::Integer(i) => i,
+            _ => panic!("wrong"),
+        }
+    }
+
+    fn to_float(self) -> Option<f64> {
+        match self {
+            Self::Float(f) => f,
+            _ => panic!("wrong"),
+        }
+    }
 }
 
 impl<'a> Encode<'a, Postgres> for Value {
@@ -16,8 +32,19 @@ impl<'a> Encode<'a, Postgres> for Value {
         match self {
             Self::Integer(opt_i) => <Option<i64> as Encode<'a, Postgres>>::encode(*opt_i, buf),
             Self::Float(opt_f) => <Option<f64> as Encode<'a, Postgres>>::encode(*opt_f, buf),
-            // Self::Array(opt_a) => <Option<Vec<Value>> as Encode<'a, Postgres>>::encode_by_ref(opt_a, buf),
-            Self::Array(_opt_a) => todo!("The thing above ^^ doesn't work. What kind of type trick we need?"),
+            Self::Array(Some(ary)) => match ary.first() {
+                Some(Value::Integer(_)) => {
+                    let ints = ary.into_iter().map(|i| i.clone().to_int()).collect();
+                    <Vec<Option<i64>> as Encode<'a, Postgres>>::encode(ints, buf)
+                }
+                Some(Value::Float(_)) => {
+                    let floats = ary.into_iter().map(|f| f.clone().to_float()).collect();
+                    <Vec<Option<f64>> as Encode<'a, Postgres>>::encode(floats, buf)
+                }
+                Some(Value::Array(_)) => unreachable!(),
+                None => <Vec<Option<f64>> as Encode<'a, Postgres>>::encode(vec![], buf),
+            },
+            Self::Array(None) => <Vec<Option<f64>> as Encode<'a, Postgres>>::encode(vec![], buf),
         }
     }
 
@@ -25,7 +52,13 @@ impl<'a> Encode<'a, Postgres> for Value {
         match self {
             Self::Integer(_) => Some(<i64 as Type<Postgres>>::type_info()),
             Self::Float(_) => Some(<f64 as Type<Postgres>>::type_info()),
-            Self::Array(_) => todo!("???"),
+            Self::Array(Some(ary)) => match ary.first() {
+                Some(Value::Integer(_)) => Some(<Vec<i64> as Type<Postgres>>::type_info()),
+                Some(Value::Float(_)) => Some(<Vec<f64> as Type<Postgres>>::type_info()),
+                Some(Value::Array(_)) => unreachable!(),
+                None => Some(<Vec<f64> as Type<Postgres>>::type_info()),
+            },
+            Self::Array(None) => Some(<Vec<f64> as Type<Postgres>>::type_info()),
         }
     }
 }
