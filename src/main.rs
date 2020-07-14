038d8e4d27a5d009b818967354cb002bf14d01b9
Julius de Bruijn <julius+github@nauk.io>
2020-07-14 23:55:57 +0200

    Explain the match

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,16 @@ async fn main() -> anyhow::Result<()> {
     let data: Vec<Option<i64>> = sqlx::query("PRAGMA table_info (\"Test\")")
         .try_map(
             |row: SqliteRow| match row.try_get_raw("dflt_value").unwrap().to_owned() {
-                vr if vr.type_info().name() == "NULL" => Ok(None),
-                vr if vr.type_info().name() == "INTEGER" => Ok(vr.decode()),
+                // This works, but is impossible to guess right due to the name
+                // being always `NULL`.
+                //
+                // The underlying column is giving the default value for the
+                // column it describes. This can be null or of any type.
+                //
+                // Here it's `Option<i64>` so we can use the `decode_unchecked`.
+                // It gets trickier to cast when having some other type as the
+                // default value.
+                vr if vr.type_info().name() == "NULL" => Ok(vr.decode_unchecked()),
                 vr => panic!(vr.type_info().name().to_string()),
             },
         )
