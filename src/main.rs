cfc50fc7d8085ba02a059f76d49a63817f2ab169
Julius de Bruijn <julius+github@nauk.io>
2020-07-14 18:46:09 +0200

    Default value, int/null problem

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,18 @@
 use sqlx::{
-    mysql::{MySqlArguments, MySqlConnection, MySqlRow},
-    query::Query,
-    Connect, FromRow, MySql, Row,
+    sqlite::{SqliteConnection, SqliteRow},
+    Connection, Row,
 };
 
-enum Value {
-    Integer(u64),
-    Boolean(bool),
-}
-
-trait Bind {
-    fn bind_value(self, value: Value) -> Self
-    where
-        Self: Sized;
-}
-
-impl<'a> Bind for Query<'a, MySql, MySqlArguments> {
-    fn bind_value(self, value: Value) -> Self {
-        match value {
-            Value::Integer(i) => self.bind(i),
-            Value::Boolean(b) => self.bind(b),
-        }
-    }
-}
-
-#[async_std::main]
+#[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut conn = MySqlConnection::connect("mysql://root:password@localhost:3306/sqlx").await?;
+    let mut conn = SqliteConnection::connect("sqlite:test.db").await?;
 
-    let query = sqlx::query("SELECT ?").bind_value(Value::Integer(1));
+    let data: Vec<Option<i64>> = sqlx::query("PRAGMA table_info (\"Test\")")
+        .try_map(|row: SqliteRow| Ok(row.get("dflt_value")))
+        .fetch_all(&mut conn)
+        .await?;
 
-    let rows: Vec<Value> = query.map(|row| Value::Integer(row.get(0))).fetch_all(&mut conn).await?;
+    dbg!(data);
 
     println!("Hello, world!");
     Ok(())
